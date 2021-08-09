929db985fcdf090793ee3c3a5e2f1a09d8644bf2
Vladimir N. Silyaev <vsilyaev@gmail.com>
2021-08-08 18:29:05 -0700

    fixup! qemu build scripts

--- build_w64.sh
+++ build_w64.sh
@@ -94,6 +94,7 @@ case $target in
         do_w64_qemu_release
         ;;
 esac
+cp -p LICENSE $release_dir
 mv qemu-img${_exe} $release_dir
 if [ -f qemu-system-x86_64${_exe} ]; then
     mv qemu-system-x86_64${_exe} $release_dir
